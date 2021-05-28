3e5a894519ac53d8adfac912be0d78edc6f841f0
Özge Odabaş <180315051@ogr.cbu.edu.tr>
2021-05-28 20:42:51 +0300

    Job Position entity created

---
+++ hrms/src/main/java/kodlama/io/hrms/entities/JobPosition.java
@@ -0,0 +1,40 @@
+package kodlama.io.hrms.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="job_positions")
+public class JobPosition {
+	@Id
+	@GeneratedValue
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="title")
+	private String title;
+	
+	public JobPosition() {
+		
+	}
+
+	public JobPosition(int id, String title) {
+		super();
+		this.id = id;
+		this.title = title;
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
